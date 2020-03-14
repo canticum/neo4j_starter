bf8408cd6e9d7fcaf4320e1c5c83ab289f5589bf
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2020-03-14 13:03:10 +0800

    add EULAR or TODAY time.

--- src/main/java/info/muspoe/test/Main.java
+++ src/main/java/info/muspoe/test/Main.java
@@ -1,4 +1,3 @@
-
 package info.muspoe.test;
 
 import info.muspoe.test.neo4j.SevenBridges;
@@ -16,13 +15,24 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
 
+        var time = switch (args.length == 0 ? "EULAR" : args[0]) {
+            case "TODAY" ->
+                SevenBridges.TODAY;
+            default ->
+                SevenBridges.EULAR;
+        };
+
         try (var sb = new SevenBridges()) {
-            IntStream.rangeClosed(1, 7)
+            System.out.println("Time=" + (time == SevenBridges.EULAR ? "EULAR" : "TODAY"));
+            sb.reset_graph();
+            sb.create_graph(time);
+            var n = sb.numberOfBridges();
+            IntStream.rangeClosed(1, n)
                     .peek(i -> System.out.printf("* Bridges: %d, start from:\n", i))
                     .forEach(i
                             -> Stream.of("left", "up", "down", "right")
                             .peek(p -> System.out.print("\t" + p.toUpperCase() + "\t= "))
-                            .map(p -> sb.count(p, i))
+                            .map(p -> sb.path_count(p, i))
                             .forEach(System.out::println));
         }
     }
--- src/main/java/info/muspoe/test/neo4j/SevenBridges.java
+++ src/main/java/info/muspoe/test/neo4j/SevenBridges.java
@@ -13,6 +13,9 @@ import org.neo4j.driver.Values;
  */
 public class SevenBridges implements AutoCloseable {
 
+    public static final int EULAR = 0;
+    public static final int TODAY = 1;
+
     private Driver driver;
 
     public SevenBridges() {
@@ -33,13 +36,60 @@ public class SevenBridges implements AutoCloseable {
         }
     }
 
-    public int count(String place, Integer num) {
+    public void reset_graph() {
+        try (var session = driver.session()) {
+            session.run("MATCH(n:Place) DETACH DELETE n;");
+        }
+    }
+
+    public void create_graph(int n) {
+
+        var query = switch (n) {
+            case TODAY:
+                yield """
+                CREATE 
+                    (left:Place{name:"left"}),
+                    (right:Place{name:"right"}),
+                    (up:Place{name:"up"}),
+                    (down:Place{name:"down"}),
+                    (left)-[:Bridge]->(up),
+                    (left)-[:Bridge]->(down),
+                    (left)-[:Bridge]->(right),
+                    (right)-[:Bridge]->(up),
+                    (right)-[:Bridge]->(down)
+                RETURN left, right , up, down;
+                """;
+            case EULAR:
+            default:
+                yield """
+                CREATE 
+                    (left:Place{name:"left"}),
+                    (right:Place{name:"right"}),
+                    (up:Place{name:"up"}),
+                    (down:Place{name:"down"}),
+                    (left)-[:Bridge]->(up),
+                    (up)-[:Bridge]->(left),
+                    (left)-[:Bridge]->(down),
+                    (down)-[:Bridge]->(left),
+                    (left)-[:Bridge]->(right),
+                    (right)-[:Bridge]->(up),
+                    (right)-[:Bridge]->(down)
+                RETURN left, right , up, down;
+                """;
+        };
+
+        try (var session = driver.session()) {
+            session.run(query);
+        }
+    }
+
+    public int path_count(String place, Integer num) {
 
         var params = Values.parameters("place", place, "num", num);
-        try (Session session = driver.session()) {
+        try (var session = driver.session()) {
             return session.run("""
                         MATCH (p)
-                        WHERE p.position=$place
+                        WHERE p.name=$place
                         CALL apoc.path.expand(p, "", "", $num, $num)
                         YIELD path
                         RETURN count(path);
@@ -48,7 +98,18 @@ public class SevenBridges implements AutoCloseable {
                     .get("count(path)")
                     .asInt();
         }
+    }
 
+    public int numberOfBridges() {
+        try (var session = driver.session()) {
+            return session.run("""
+                        MATCH ()-[bridge:Bridge]->()
+                        RETURN count(bridge);
+                        """)
+                    .single()
+                    .get("count(bridge)")
+                    .asInt();
+        }
     }
 
     @Override
